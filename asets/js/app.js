7e64bcd290d540d87386162a473d2b3003a48178
RAM-413 <ramsharma22422@gmail.com>
2023-10-12 16:39:54 +0530

    first-commit

--- asets/js/app.js
+++ asets/js/app.js
@@ -28,6 +28,90 @@ $('.slide').slick({
 
     ]
 });
+$('.slider').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    prevArrow: '.1prev-arrow',
+    nextArrow: '.2nxt-arrow',
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+
+            }
+        },
+
+    ]
+});
+
+
+$('.slick2').slick({
+    dots: false,
+    infinite: true,
+    speed: 3000,
+    autoplay: true,
+    autoplaySpeed: 0,
+    cssEase: 'Linear',
+    slidesToShow: 8,
+    slidesToScroll: 1,
+
+    responsive: [{
+        breakpoint: 1100,
+        settings: {
+            slidesToShow: 6,
+            slidesToScroll: 1,
+            autoplaySpeed: 0,
+            cssEase: 'Linear',
+            infinite: true,
+            dots: false,
+        }
+    },
+    {
+        breakpoint: 900,
+        settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 0,
+            cssEase: 'Linear',
+            speed: 1000,
+        }
+    },
+    {
+        breakpoint: 650,
+        settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            autoplaySpeed: 0,
+            cssEase: 'Linear',
+        }
+    },
+    {
+        breakpoint: 580,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            autoplaySpeed: 0,
+            cssEase: 'Linear',
+        }
+    },
+    {
+        breakpoint: 480,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            autoplaySpeed: 0,
+            cssEase: 'Linear',
+        }
+    }
+    ]
+
+})
+
 
 let AddBtn = document.querySelector(".add");
 let SubBtn = document.querySelector(".sub");
@@ -51,3 +135,40 @@ var x = setInterval(function () {
         document.getElementById("demo").innerHTML = "EXPIRED";
     }
 }, 1000);
+
+
+
+//  let icon = document.querySelector(".icon");
+// let page = document.querySelector(".mobile-view");
+// icon.addEventListener("click", function () {
+//     if (page.classList !== document.querySelector(".right-0")) {
+//         page.classList.toggle("right-0");
+//     }
+//  })
+
+// let label = document.querySelector(".icon");
+// let menu = document.querySelector(".mobile-view");
+// let open = 0;
+// aTags.forEach((c) => {
+//     c.addEventListener("click", () => {
+//         if (open != 0) {
+//             menu.style.right = '-100%';
+//             document.body.classList.remove("overflow-y-hidden");
+//             label.classList.remove("label-switch"); open--;
+//         }
+//     })
+// })
+
+// label.addEventListener("click", () => {
+//     if (open == 0) {
+//         menu.style.right = 0;
+//         document.body.classList.add("overflow-y-hidden");
+//         label.classList.add("label-switch");
+//         open++;
+//     } else {
+//         menu.style.right = '-100%';
+//         document.body.classList.remove("overflow-y-hidden");
+//         label.classList.remove("label-switch");
+//         open--;
+//     }
+// });
